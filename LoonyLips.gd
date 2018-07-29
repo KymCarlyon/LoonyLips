42c8a9fe4915071ca6145a937756e20d2b6ce55c
KIDS\kids <kymberlea.carlyon@gmail.com>
2018-07-29 13:14:42 +0100

    L11 comp

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
+var player_words = [] 
+var prompt = ["a name", " a food", "feeling", "something", ]
+var story = "Once upon a time %s ate some %s and felt very %s. He/She went upstairs and got %s."
+	
 func _ready():
-	var prompt = ["Myka", "popcorn", "full", "book", ]
-	var story = "Once upon a time %s ate some %s and felt very %s. She went upstairs and read a %s."
-	$Blackboard/StoryText.bbcode_text = story % prompt
+	$Blackboard/StoryText.text = story % prompt
+	$Blackboard/StoryText.text = ("Welcome to Loony Lips!\n\nWe're going to tell a story and have a lovely time!\nCan I have " + prompt[player_words.size()] + ", please?")
 	$Blackboard/TextBox.text = ""
-
+	
+	
 func _on_TextureButton_pressed():
-	var new_text = $Blackboard/TextBox.get_text()
-	_on_TextBox_text_entered(new_text)
-
-
+		var new_text = $Blackboard/TextBox.get_text()
+		_on_TextBox_text_entered(new_text)
+		
 func _on_TextBox_text_entered(new_text):
-	$Blackboard/StoryText.text = new_text
+	player_words.append(new_text)
 	$Blackboard/TextBox.text = ""
+	check_player_word_length()
+		
+func prompt_player():
+	$Blackboard/StoryText.text = ("Can I have " + prompt[player_words.size()] + ", please?")
+	
+func check_player_word_length():
+	if player_words.size() == prompt.size():
+		tell_story()
+	else:
+		prompt_player()
+	
+func tell_story():
+		$Blackboard/StoryText.text = story % player_words
